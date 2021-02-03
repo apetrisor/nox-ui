aaf284f68c8df3eb4094b1f844c666883f829286
Alex Petrisor <alexgpetrisor@gmail.com>
2021-02-03 15:50:28 +0000

    Carousel component

---
+++ src/components/Carousel.svelte
@@ -0,0 +1,89 @@
+<script>
+	import Siema from 'siema';
+	import {onMount} from 'svelte';
+
+	export let perPage = 3;
+	export let controls = true;
+	export let threshold = 20;
+	export let rtl = false;
+
+	let siema;
+	let controller;
+
+	onMount(() => {
+		let is_mobile = 'ontouchstart' in document.documentElement ? true : false;
+		let props = is_mobile
+			? {
+					draggable: true,
+					multipleDrag: true,
+					duration: 200,
+					easing: 'ease-out',
+			  }
+			: {
+					draggable: false,
+					multipleDrag: false,
+					duration: 700,
+					easing: 'cubic-bezier(0.4, 0, 0, 1)',
+			  };
+
+		controller = new Siema({
+			selector: siema,
+			perPage,
+			loop: true,
+			threshold,
+			rtl,
+			...props,
+		});
+
+		return () => controller.destroy();
+	});
+
+	const goLeft = () => controller.prev(controller.perPage);
+	const goRight = () => controller.next(controller.perPage);
+</script>
+
+<style>
+	.carousel {
+		position: relative;
+		width: 100%;
+		justify-content: center;
+		align-items: center;
+	}
+
+	button {
+		position: absolute;
+		width: 40px;
+		height: 40px;
+		top: 50%;
+		z-index: 50;
+		margin-top: -20px;
+		border: none;
+		background-color: transparent;
+	}
+
+	button:focus {
+		outline: none;
+	}
+
+	.left {
+		left: 2vw;
+	}
+
+	.right {
+		right: 2vw;
+	}
+</style>
+
+<div class="carousel">
+	<div class="slides" bind:this={siema}>
+		<slot />
+	</div>
+	{#if controls}
+		<button class="left" on:click={goLeft} aria-label="left">
+			<slot name="left-control" />
+		</button>
+		<button class="right" on:click={goRight} aria-label="right">
+			<slot name="right-control" />
+		</button>
+	{/if}
+</div>
--- src/index.js
+++ src/index.js
@@ -2,3 +2,4 @@ export { default as Image } from './components/Image.svelte';
 export { default as MultiSelect } from './components/MultiSelect.svelte';
 export { default as Sticky } from './components/Sticky.svelte';
 export { default as Card } from './components/Card.svelte';
+export { default as Carousel } from './components/Carousel.svelte';
