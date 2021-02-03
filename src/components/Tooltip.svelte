03f3a77c5a13fbfed7e8fc5678686eb19363ed81
Alex Petrisor <alexgpetrisor@gmail.com>
2021-02-03 16:16:45 +0000

    Tooltip component

---
+++ src/components/Tooltip.svelte
@@ -0,0 +1,50 @@
+<script>
+	export let content;
+	export let top = false;
+	export let bottom = false;
+	export let width = undefined;
+
+	let style = '';
+	if (width) style = `width: ${width}`;
+</script>
+
+<style lang="sass">
+	.tooltip
+		position: relative
+		display: inline-block
+		cursor: pointer
+
+		.content
+			position: absolute
+			visibility: hidden
+			opacity: 0
+			transform: translate3d(-50%, 0px, 0px)
+			padding: 15px
+			background: #fff
+			transition: all 0.3s cubic-bezier(0.4, 0, 0, 1)
+			width: 300px
+			max-width: 100vh
+			text-align: center
+			border-radius: 6px
+			pointer-events: none
+			&.top
+				top: 0
+				left: 50%
+			&.bottom
+				bottom: 0
+				left: 50%
+
+		&:hover
+			.content
+				opacity: 1
+				visibility: visible
+			.top
+				transform: translate3d(-50%, -80%, 0)
+			.bottom
+				transform: translate3d(-50%, 80%, 0)
+</style>
+
+<div class="tooltip">
+	<slot />
+	<div class="content shadow-2xl text-sm" class:top class:bottom {style}>{content}</div>
+</div>
