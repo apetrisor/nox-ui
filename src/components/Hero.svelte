a61ead126a5ac6467c1338d1ad63619efa359c0e
Alex Petrisor <alexgpetrisor@gmail.com>
2021-02-19 14:56:09 +0000

    Hero bg tweaks, support for overlay

--- src/components/Hero.svelte
+++ src/components/Hero.svelte
@@ -3,6 +3,8 @@
 	export let subtitle;
 	export let bg = false;
 	export let overlay = false;
+
+	let screenWidth = 768;
 </script>
 
 <style lang="sass">
@@ -11,35 +13,38 @@
 		position: relative
 		text-align: center
 		padding: 80px 0px
-		h1
-			margin-bottom: 20px
-		.content
-			margin-top: 40px
-
-		.bg-mobile, .bg-desktop
+		&.bg
 			background-size: cover
 			background-position: center
-		.bg-mobile
 			height: 400px
-		.bg-desktop
-			height: 640px
-			display: none
+			@media (min-width: 768px)
+				height: 640px
 
-		@media (min-width: 768px)
-			.bg-mobile
-				display: none
-			.bg-desktop
-				display: block
+	.overlay
+		position: absolute
+		z-index: 1
+		top: 0
+		bottom: 0
+		left: 0
+		right: 0
+		background: linear-gradient(180deg, rgba(0, 0, 0, 0) 0, rgba(0, 0, 0, 0.15) 100%)
+
+	h1
+		margin-bottom: 20px
+
+	.content
+		margin-top: 40px
+		position: relative
+		z-index: 2
 </style>
 
-<div class="nox-hero">
+<svelte:window bind:innerWidth={screenWidth} />
+
+<div class="nox-hero" class:bg style={bg ? `background-image:url(${screenWidth < 768 ? bg.mobile : bg.desktop})` : null}>
 	{#if title}<h1>{title}</h1>{/if}
 	{#if subtitle}<h2>{subtitle}</h2>{/if}
 
-	{#if bg}
-		<div class="bg-mobile" style="background-image:url({bg.mobile})" />
-		<div class="bg-desktop" style="background-image:url({bg.desktop})" />
-	{/if}
+	{#if overlay}<div class="overlay" />{/if}
 
 	{#if $$slots.default}
 		<div class="content">
