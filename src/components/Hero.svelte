31febaaa29d757994a98a0474a267ae16d72eae9
Alex Petrisor <alexgpetrisor@gmail.com>
2021-02-19 13:57:55 +0000

    Basic Hero component

---
+++ src/components/Hero.svelte
@@ -0,0 +1,27 @@
+<script>
+	export let title;
+	export let subtitle;
+</script>
+
+<style lang="sass">
+	@import '../styles/variables'
+	.nox-hero
+		text-align: center
+		padding: 80px 0px
+		h1
+			margin-bottom: 20px
+		.content
+			margin-top: 40px
+</style>
+
+<div class="nox-hero">
+	<h1>{title}</h1>
+	{#if subtitle}
+		<h2>{subtitle}</h2>
+	{/if}
+	{#if $$slots.default}
+		<div class="content">
+			<slot />
+		</div>
+	{/if}
+</div>
