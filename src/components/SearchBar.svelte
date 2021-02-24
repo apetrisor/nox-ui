2fbf59fcceb04d4e4f6ac61c3c2b160ea4ae8710
Alex Petrisor <alexgpetrisor@gmail.com>
2021-02-24 16:07:52 +0000

    SearchBar component

---
+++ src/components/SearchBar.svelte
@@ -0,0 +1,60 @@
+<script>
+	import SearchIcon from 'svelte-feather-icons/src/icons/SearchIcon.svelte';
+	import XIcon from 'svelte-feather-icons/src/icons/XIcon.svelte';
+	import {createEventDispatcher} from 'svelte';
+	const dispatch = createEventDispatcher();
+
+	export let value;
+	export let small;
+
+	const handleClear = () => (value = '');
+	const handleSubmit = () => dispatch('submit', value);
+</script>
+
+<style lang="scss" global>
+	@import '../styles/variables';
+
+	.nox-search-bar {
+		width: 100%;
+		display: flex;
+		color: $gray-700;
+		background: $gray-200;
+		border-radius: 8px;
+		border: 1px solid transparent;
+
+		button {
+			width: 60px;
+			padding: 18px;
+			flex-shrink: 0;
+		}
+
+		input {
+			padding: 10px 0;
+			width: 100%;
+			background: none;
+			font-size: 18px;
+		}
+
+		&.small {
+			button {
+				width: 44px;
+				padding: 12px;
+			}
+			input {
+				font-size: 16px;
+			}
+		}
+	}
+</style>
+
+<form class="nox-search-bar" class:small on:submit|preventDefault={handleSubmit}>
+	<button type="submit" title="Search Undissimo">
+		<SearchIcon />
+	</button>
+	<input placeholder="Search underwear" bind:value />
+	{#if value}
+		<button title="Clear Search" on:click={handleClear}>
+			<XIcon />
+		</button>
+	{/if}
+</form>
--- src/index.js
+++ src/index.js
@@ -7,6 +7,7 @@ export {default as Link} from './components/Link.svelte';
 export {default as MultiSelect} from './components/MultiSelect.svelte';
 export {default as Pagination} from './components/Pagination.svelte';
 export {default as Rating} from './components/Rating.svelte';
+export {default as SearchBar} from './components/SearchBar.svelte';
 export {default as Sticky} from './components/Sticky.svelte';
 export {default as ShareBox} from './components/ShareBox.svelte';
 export {default as Tooltip} from './components/Tooltip.svelte';
