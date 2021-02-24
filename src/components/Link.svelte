780aa37b7121a16a17ec038ed03ff822ef2bee1c
Alex Petrisor <alexgpetrisor@gmail.com>
2021-02-24 14:22:16 +0000

    Link component

---
+++ src/components/Link.svelte
@@ -0,0 +1,19 @@
+<script>
+	export let href = '#';
+	export let external = false;
+	export let scroll = false;
+
+	let className;
+	export {className as class};
+
+	const doScroll = e => {
+		e.preventDefault();
+		document.querySelector(scroll).scrollIntoView({behavior: 'smooth'});
+	};
+
+	$: target = external ? '_blank' : null;
+	$: rel = external ? 'nofollow noopener noreferrer' : null;
+	$: click = scroll ? doScroll : null;
+</script>
+
+<a on:click={click} {href} {target} {rel} class={className}><slot /></a>
--- src/index.js
+++ src/index.js
@@ -3,6 +3,7 @@ export {default as Carousel} from './components/Carousel.svelte';
 export {default as Header} from './components/Header.svelte';
 export {default as Hero} from './components/Hero.svelte';
 export {default as Image} from './components/Image.svelte';
+export {default as Link} from './components/Link.svelte';
 export {default as MultiSelect} from './components/MultiSelect.svelte';
 export {default as Pagination} from './components/Pagination.svelte';
 export {default as Rating} from './components/Rating.svelte';
