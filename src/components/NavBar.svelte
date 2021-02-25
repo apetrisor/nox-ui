640546367924c3496f29757390979c4cd03f6147
Alex Petrisor <alexgpetrisor@gmail.com>
2021-02-25 13:25:45 +0000

    Updater sticky NavBar behaviour

--- src/components/NavBar.svelte
+++ src/components/NavBar.svelte
@@ -18,43 +18,53 @@
 
 	const toggleMobileMenu = () => (mobileMenuOpen = !mobileMenuOpen);
 
-	let screenWidth = 1280;
 	let scrollY;
-
-	$: offsetTop = screenWidth < 768 ? 0 : 115;
-	$: stuck = scrollY > offsetTop;
+	$: stuck = scrollY > 60;
 </script>
 
 <style lang="scss" global>
 	@import '../styles/variables';
 
 	.nox-navbar {
-		border-bottom: 1px solid $gray-200;
 		background: #fff;
 		transition: all 0.3s;
 		position: relative;
+		.inner {
+			border-bottom: 1px solid $gray-200;
+		}
 
 		&.sticky {
-			padding-top: 64px;
-			.header {
-				position: absolute;
+			height: 64px;
+			.inner {
+				height: 64px;
+				position: fixed;
 				width: 100%;
 				top: 0px;
 				z-index: 1000;
-			}
-			@include screen-md {
-				padding-top: 0px;
-				margin-top: 74px;
-				.header {
-					transform: translateY(-74px);
+				background: #fff;
+				transition: height 0.3s, box-shadow 0.3s;
+				.submenu {
+					transition: all 0.3s;
 				}
 			}
+
 			&.stuck {
-				.header {
-					position: fixed;
+				.inner {
 					box-shadow: 0px 0px 30px rgba(0, 0, 0, 0.07);
-					transform: translateY(0px);
-					border-bottom: 1px solid $gray-200;
+					.submenu {
+						visibility: hidden;
+						opacity: 0;
+					}
+				}
+			}
+
+			@include screen-md {
+				height: 115px;
+				.inner {
+					height: 115px;
+				}
+				&.stuck .inner {
+					height: 74px;
 				}
 			}
 		}
@@ -67,28 +77,23 @@
 		}
 
 		.header {
-			height: 64px;
 			padding: 10px 0;
-			background: #fff;
-			transition: transform 0.4s;
 			.container {
 				display: flex;
 				align-items: center;
 				justify-content: space-between;
 			}
-
 			.menu-button {
 				width: 44px;
 				height: 44px;
 				padding: 10px;
 				margin-right: -10px;
-				@include screen-md {
-					display: none;
-				}
 			}
 			@include screen-md {
-				height: 74px;
 				padding: 15px 0;
+				.menu-button {
+					display: none;
+				}
 			}
 		}
 
@@ -121,6 +126,7 @@
 			bottom: 0;
 			overflow-y: scroll;
 			background: #fff;
+			padding: 20px 0;
 			@include screen-md {
 				display: none;
 			}
@@ -137,38 +143,40 @@
 	}
 </style>
 
-<svelte:window bind:scrollY bind:innerWidth={screenWidth} />
+<svelte:window bind:scrollY />
 
 <div class="nox-navbar" class:sticky class:stuck>
-	<div class="header">
-		<div class="container">
-			<div>
-				<a href="/"><slot name="logo">Company Logo</slot></a>
-			</div>
-
-			<div class="search-bar">
-				<SearchBar small stretch {placeholder} on:search />
-			</div>
-
-			<button class="menu-button" on:click={toggleMobileMenu} type="button">
-				{#if mobileMenuOpen}
-					<XIcon />
-				{:else}
-					<MenuIcon />
-				{/if}
-			</button>
-		</div>
-	</div>
-
-	{#if submenu}
-		<div class="submenu">
+	<div class="inner">
+		<div class="header">
 			<div class="container">
-				{#each submenu as page}
-					<a href={page.path} class:active={page.path === path}>{page.name}</a>
-				{/each}
+				<div>
+					<a href="/"><slot name="logo">Company Logo</slot></a>
+				</div>
+
+				<div class="search-bar">
+					<SearchBar small stretch {placeholder} on:search />
+				</div>
+
+				<button class="menu-button" on:click={toggleMobileMenu} type="button">
+					{#if mobileMenuOpen}
+						<XIcon />
+					{:else}
+						<MenuIcon />
+					{/if}
+				</button>
 			</div>
 		</div>
-	{/if}
+
+		{#if submenu}
+			<div class="submenu">
+				<div class="container">
+					{#each submenu as page}
+						<a href={page.path} class:active={page.path === path}>{page.name}</a>
+					{/each}
+				</div>
+			</div>
+		{/if}
+	</div>
 
 	{#if mobileMenuOpen}
 		<div transition:fly={{y: -20}} class="mobile-menu">
