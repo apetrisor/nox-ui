12f1c0a579e006184ffeb56e3963e8097dce5b3c
Alex Petrisor <alexgpetrisor@gmail.com>
2021-03-10 14:33:35 +0000

    Check component

---
+++ src/components/Check.svelte
@@ -0,0 +1,33 @@
+<script>
+	import FaRegCheckCircle from 'svelte-icons/fa/FaRegCheckCircle.svelte';
+	import FaRegTimesCircle from 'svelte-icons/fa/FaRegTimesCircle.svelte';
+	export let checked;
+
+	let className = '';
+	export {className as class};
+</script>
+
+<style>
+	.nox-check {
+		display: inline-block;
+		vertical-align: middle;
+		width: 22px;
+		height: 22px;
+		color: #f35858;
+		&.checked {
+			color: #41dc4e;
+		}
+	}
+</style>
+
+<div class="nox-check {className}" class:checked>
+	{#if checked}
+		<slot name="checked">
+			<FaRegCheckCircle />
+		</slot>
+	{:else}
+		<slot name="unchecked">
+			<FaRegTimesCircle />
+		</slot>
+	{/if}
+</div>
--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
 export {default as Card} from './components/Card.svelte';
 export {default as Carousel} from './components/Carousel.svelte';
+export {default as Check} from './components/Check.svelte';
 export {default as Footer} from './components/Footer.svelte';
 export {default as GoogleAnalytics} from './components/external/GoogleAnalytics.svelte';
 export {default as Header} from './components/Header.svelte';
