4a55ba375018c5143b2a600011e96144b994ab44
Alex Petrisor <alexgpetrisor@gmail.com>
2021-03-28 19:10:21 +0100

    Removed Footer component

--- src/index.js
+++ src/index.js
@@ -3,7 +3,6 @@ export {default as Carousel} from './components/Carousel.svelte';
 export {default as Check} from './components/Check.svelte';
 export {default as CheckBox} from './components/CheckBox.svelte';
 export {default as Columns} from './components/Columns.svelte';
-export {default as Footer} from './components/Footer.svelte';
 export {default as Genius} from './components/external/Genius.svelte';
 export {default as GoogleAnalytics} from './components/external/GoogleAnalytics.svelte';
 export {default as Header} from './components/Header.svelte';
