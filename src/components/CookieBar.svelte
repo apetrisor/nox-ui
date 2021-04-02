13f2e3c65dee021d46c707cf86e5c8ef0532589a
Alex Petrisor <alexgpetrisor@gmail.com>
2021-04-02 14:11:08 +0100

    CookieBar component

---
+++ src/components/CookieBar.svelte
@@ -0,0 +1,47 @@
+<script>
+	import {onMount} from 'svelte';
+
+	let visible = false;
+
+	onMount(() => {
+		if (localStorage && !localStorage.getItem('acceptedCookies')) {
+			visible = true;
+		}
+	});
+
+	const handleAccept = () => {
+		if (localStorage) localStorage.setItem('acceptedCookies', true);
+		visible = false;
+	};
+</script>
+
+<style lang="scss" global>
+	.nox-cookie-bar {
+		color: #fff;
+		background: #1d1d29;
+		position: fixed;
+		bottom: 0;
+		left: 0;
+		right: 0;
+		z-index: 9999;
+		padding: 1rem;
+		display: flex;
+		align-items: center;
+		justify-content: center;
+
+		> span {
+			margin-left: 3rem;
+		}
+	}
+</style>
+
+{#if visible}
+	<div class="nox-cookie-bar">
+		<slot>We're using little cookies to improve your awesome surfing experience</slot>
+		<span on:click={handleAccept}>
+			<slot name="button">
+				<button>Ok</button>
+			</slot>
+		</span>
+	</div>
+{/if}
--- src/index.js
+++ src/index.js
@@ -3,6 +3,7 @@ export {default as Carousel} from './components/Carousel.svelte';
 export {default as Centered} from './components/Centered.svelte';
 export {default as Check} from './components/Check.svelte';
 export {default as CheckBox} from './components/CheckBox.svelte';
+export {default as CookieBar} from './components/CookieBar.svelte';
 export {default as Genius} from './components/external/Genius.svelte';
 export {default as GoogleAnalytics} from './components/external/GoogleAnalytics.svelte';
 export {default as Header} from './components/Header.svelte';
